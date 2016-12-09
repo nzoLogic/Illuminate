24c6bdfa933a5350e1670ea56669176bfed22594
nzoLogic <nzoLogic@gmail.com>
2016-12-08 19:49:28 -0800

    Have the random number generator operational that replicates the amount of keys to generate, and which child to select. Useful for .data() as well

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 console.log('app.js loaded');
-$(document).ready(function(){
+$(document).ready(function() {
 
-  console.log("jQuery online");
-  var test = $('#one');
-  $(test).data({square: 'hey'});
-  console.log(test.data(), test);
+    console.log("jQuery online");
+    var test = $('#one');
+    $(test).data({
+        square: 'hey'
+    });
+    $('.square').on('click', checkOrder);
+    randomizeOrder($('main').children());
+    blink(test);
+    //function that randomizes key:value assignments for each child of main !!FIX THIS !!!!
+    function blink(block){
+      // console.log('blink function');
+      $(block).delay(1000).addClass('lit');
+    }
+    function randomizeOrder(parent) {
+        var box = parent[Math.floor(Math.random()* parent.length)];
+        console.log($(this).data(dataMap));
+    }
 
-  $('.square').on('click', checkOrder);
-  console.log($('.game-board').children());
-  function checkOrder(){
-    console.log($(this).data('square') === 'hey');
-  }
-
-  function randomizeOrder(){
-
-  }
+    function checkOrder() {
+        console.log($(this).data('square') === 'hey');
+    }
+    function dataMap(){
+      return
+    }
 });
