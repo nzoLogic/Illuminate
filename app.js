8dcead0ae6a7d88e30458a97524d221c722e97a5
nzoLogic <nzoLogic@gmail.com>
2016-12-08 22:16:42 -0800

    have a basic way to generate keys to random divs with a copy binded to user clicks that have to match a specific order

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
 console.log('app.js loaded');
+var userKeys = [1, 'a', 3];
 $(document).ready(function() {
-
-    console.log("jQuery online");
     var test = $('#one');
     $(test).data({
-        square: 'hey'
+        'key': [1]
     });
-    $('.square').on('click', checkOrder);
-    randomizeOrder($('main').children());
+  $('.game-board').on('click',function(event){
+    // console.log($(event.target).data('key').shift());
+    compareKeys($(event.target).data('key').shift());
+  })
     blink(test);
-    //function that randomizes key:value assignments for each child of main !!FIX THIS !!!!
-    function blink(block){
-      // console.log('blink function');
-      $(block).delay(1000).addClass('lit');
+    // function that randomizes key:value assignments for each child of main !!FIX THIS !!!!
+    function blink(block) {
+        $(block).delay(1000).addClass('lit');
     }
+      //random number generator for numbers 0 through n amounts of blinks
+          //used for amount of keys needed, and random selector for the squares
     function randomizeOrder(parent) {
-        var box = parent[Math.floor(Math.random()* parent.length)];
-        console.log($(this).data(dataMap));
+        var box = parent[Math.floor(Math.random() * parent.length)];
+        parent.each(function(child) {
+            $(child).data({
+                key: [1]
+            });
+            console.log($(child).data());
+        })
     }
 
-    function checkOrder() {
-        console.log($(this).data('square') === 'hey');
+    function compareKeys(squareKey) {
+      console.log(squareKey === userKeys.shift())
     }
-    function dataMap(){
-      return
-    }
+
+
 });
