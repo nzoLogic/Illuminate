2da294bb3a7303e362a0e3d640f0c9662e0d12e9
nzoLogic <nzoLogic@gmail.com>
2016-12-11 00:05:04 -0800

    began adding circles. broken the blink function due to new children having to be considered. fix tomorrow

--- script/app.js
+++ script/app.js
@@ -2,11 +2,15 @@ console.log('app.js loaded');
 var userKeys = [],
     correctOrder = [],
     level = 1,
-    iterations = 3;
+    iterations = 3,
+    time = 2000;
 $(document).ready(function() {
-    var children = $('.game-board').children();
+  ///new children with circles. fix this
+    var children = $('.game-board').children().hasAttribute('id');
+    console.log(children);
+    var e = new Audio('./sounds/68443__pinkyfinger__piano-e.wav');
+    e.playbackRate = 2.0;
     //section for event listeners
-
     $('.begin').click(startGame);
     //constructor for making the game more modular **proto phase//implement later
     function Game(obj) {
@@ -14,17 +18,26 @@ $(document).ready(function() {
         this.level = 1,
             this.blinks = 3,
             this.correctOrder = [],
-            this.gameBoard = $('.game-board')
-    }
+            this.children = $('.game-board').children(),
+            this.time = 2000
+          }
     var Illuminate = new Game({});
+    Game.prototype.randomNumber = function(){
+      return Math.floor(Math.random() * children.length);
+
+    }
+
+    console.log(Illuminate.randomNumber());
     function allowUserClick(){
       $('.game-board').on('click', function(event) {
           var selectedId = event.target.id;
           compareKeys(selectedId);
       });
     }
+
     function blink(block) {
         $(block).addClass('lit');
+        // e.play();
         setTimeout(function() {
             $(block).removeClass('lit');
         }, 500);
@@ -32,15 +45,14 @@ $(document).ready(function() {
 
     //random number generator for numbers 0 through n amounts of blinks
     //used for amount of keys needed, and random selector for the squares
-    // randomizeOrder($('.game-board'));
-
     function startGame() {
+        levelDisplay(level);
         allowUserClick();
         numberOfIterations(iterations);
     }
 
     function randomNumber() {
-        return Math.floor(Math.random() * $('.game-board').children().length);
+        return Math.floor(Math.random() * children.length);
     }
 
     function compareKeys(selectedId) {
@@ -57,7 +69,7 @@ $(document).ready(function() {
             console.log(correctOrder);
         }
     }
-    //navigator for blink, and div sequence ordering
+    //the hub of functions that is responsible for calling the other integral functions
     function numberOfIterations(n) {
         var child = children[randomNumber()];
         if (n === 0) {
@@ -68,27 +80,37 @@ $(document).ready(function() {
             blink(child, n);
             console.log(correctOrder);
             return numberOfIterations(n - 1);
-        }, 2000);
+        }, time);
     }
     //asign value to keys
     function sequenceAndUserKeys(element) {
         correctOrder.push(element);
     }
     //resets game because of incorrect selection
-    // TODO//
     function gameOver() {
         console.log('game over')
+        level = 1;
+        iterations = 3;
+        correctOrder = [],
+        time = 1500;
     }
     //advances level and complexity
     function nextLevel() {
         level += 1;
-        console.log(level);
+        levelDisplay(level);
+        // console.log(level);
         iterations += 2;
-        console.log(iterations);
+        // console.log(iterations);
         numberOfIterations(iterations);
     }
-
+    function levelDisplay(level){
+      $('.insert').html(level);
+    }
+    //checks the condition of the round to see if victory is possible
     function roundWon() {
         return correctOrder.length === 0;
+    }
+    function showVictory(){
+
     }
 });
