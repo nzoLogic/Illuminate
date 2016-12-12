f118c0c0cf6c9b374b71a4d56f03ab8fd548c521
nzoLogic <nzoLogic@gmail.com>
2016-12-12 00:31:49 -0800

    project version

--- script/app.js
+++ script/app.js
@@ -1,64 +1,67 @@
 console.log('app.js loaded');
 var userKeys = [],
     correctOrder = [],
+    colors = ['yellow', 'green', 'red'],
     level = 1,
     iterations = 3,
     time = 2000;
 $(document).ready(function() {
-  ///new children with circles. fix this
     var children = $('.game-board').find('.circle');
-    console.log(children);
-    var e = new Audio('./sounds/68443__pinkyfinger__piano-e.wav');
-    e.playbackRate = 2.0;
-    //section for event listeners
+    pageLoad();
     $('.begin').click(startGame);
-    //constructor for making the game more modular **proto phase//implement later
-    function Game(obj) {
-        this.game = 'Illumination Theory'
-        this.level = 1,
-            this.blinks = 3,
-            this.correctOrder = [],
-            this.children = $('.game-board').children(),
-            this.time = 2000
-          }
-    var Illuminate = new Game({});
-    Game.prototype.randomNumber = function(){
-      return Math.floor(Math.random() * children.length);
+    $('.restart').click(startGame);
+
+
+    function pageLoad() {
+        $('.intro-title').css('opacity', '1');
+        setTimeout(function() {
+            $('p.description').css('opacity', '1');
+            $('.begin').delay(7000).css('opacity', '1');
+        }, 2500);
 
     }
 
-    // console.log(Illuminate.randomNumber());
-    function allowUserClick(){
-      $('.game-board').on('click', function(event) {
-          var selectedId = event.target.id;
-          console.log(selectedId);
-          compareKeys(selectedId);
-      });
+    function allowUserClick() {
+        $('.game-board').on('click', function(event) {
+            var selectedId = event.target.id;
+            console.log(selectedId);
+            compareKeys(selectedId);
+        });
     }
 
-    function blink(block) {
-        $(block).addClass('lit');
+    function blink(child) {
+        $(child).addClass('red');
         // e.play();
         setTimeout(function() {
-            $(block).removeClass('lit');
+            $(child).removeClass('red');
         }, 500);
     }
 
-    //random number generator for numbers 0 through n amounts of blinks
-    //used for amount of keys needed, and random selector for the squares
     function startGame() {
+        console.log('game started')
+        hideScreen();
         levelDisplay(level);
-        allowUserClick();
-        numberOfIterations(iterations);
-    }
-    introLights;
-    function introLights(children){
-      children.forEach(function(child){
-        blink(child);
-      })
+        introLights(3);
     }
 
+    function introLights(n) {
+        if (n === 0) {
+            blink(children);
+            allowUserClick();
+            numberOfIterations(iterations);
+            return;
+        } else {
+            blink(children[3]);
+            setTimeout(function() {
+                return introLights(n - 1);
+            }, 1500);
+        }
+    }
+
+
+
     function randomNumber() {
+        console.log(this.length)
         return Math.floor(Math.random() * children.length);
     }
 
@@ -66,59 +69,83 @@ $(document).ready(function() {
         if (selectedId !== correctOrder[0].id) {
             console.log('wrong');
             gameOver();
-        } else {
-            console.log('Correct', correctOrder);
-            correctOrder.shift();
-            if (roundWon()) {
-                console.log('round won');
-                nextLevel();
-            }
-            console.log(correctOrder);
-        }
-    }
-    //the hub of functions that is responsible for calling the other integral functions
-    function numberOfIterations(n) {
-        var child = children[randomNumber()];
-        console.log(child);
-        if (n === 0) {
             return;
         }
-        sequenceAndUserKeys(child);
-        setTimeout(function() {
-            blink(child, n);
-            // console.log(correctOrder);
-            return numberOfIterations(n - 1);
-        }, time);
-    }
-    //asign value to keys
-    function sequenceAndUserKeys(element) {
-        correctOrder.push(element);
-    }
-    //resets game because of incorrect selection
-    function gameOver() {
-        console.log('game over')
-        level = 1;
-        iterations = 3;
-        correctOrder = [],
-        time = 1500;
-    }
-    //advances level and complexity
-    function nextLevel() {
-        level += 1;
-        levelDisplay(level);
-        // console.log(level);
-        iterations += 2;
-        // console.log(iterations);
-        numberOfIterations(iterations);
-    }
-    function levelDisplay(level){
-      $('.insert').html(level);
-    }
-    //checks the condition of the round to see if victory is possible
-    function roundWon() {
-        return correctOrder.length === 0;
-    }
-    function showVictory(){
+        else {
+          correctOrder.shift();
+        }
+        if (roundWon()) {
+            showVictory(level);
+            setTimeout(function() {
+              nextLevel();
+            }, 1500);
+            console.log('round won');
+        }
 
+        console.log(correctOrder);
     }
+
+//the hub of functions that is responsible for calling the other functions
+function numberOfIterations(n) {
+    var child = children[randomNumber()];
+    if (n === 0) {
+        return;
+    }
+    console.log(child);
+    sequenceAndUserKeys(child);
+    setTimeout(function() {
+        blink(child);
+        // console.log(correctOrder);
+        return numberOfIterations(n - 1);
+    }, time);
+}
+//asign value to keys
+function sequenceAndUserKeys(element) {
+    correctOrder.push(element);
+}
+//resets game because of incorrect selection
+
+function gameOver() {
+    level = 1;
+    iterations = 3;
+    time = 1500,
+    correctOrder = [];
+    showScreen();
+    $('.outcome').html('GAME OVER').fadeIn(2000).fadeOut(2000);
+    setTimeout(function(){
+      location.reload();
+    }, 5000);
+}
+//advances level and complexity
+function nextLevel() {
+    level += 1;
+    levelDisplay(level);
+    hideScreen();
+    // console.log(level);
+    iterations += 1;
+    // console.log(iterations);
+    numberOfIterations(iterations);
+}
+function levelDisplay(level) {
+    $('.insert').html(level);
+}
+//checks the condition of the round to see if victory is possible
+function roundWon(id) {
+    return correctOrder.length === 0;
+}
+
+function showVictory(level) {
+    $('.content').css('height', '40%');
+    $('.outcome').html('Success!');
+
+}
+
+function showScreen() {
+    $('.content').css('height', '100%');
+}
+
+function hideScreen() {
+    $('.display').css('height', '0');
+    $('.content').css('height', '0');
+}
 });
